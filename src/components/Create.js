51b964ee67a2b278143507920b18ca3533ce7aef
chrisharvie <christopherharvie91@gmail.com>
2024-02-11 22:17:52 +0000

    update create

--- src/components/Create.js
+++ src/components/Create.js
@@ -152,11 +152,10 @@ const Create = (artick) => {
 
   return (
     <div>
-      <Navigation web3Handler={web3Handler} account={account} />
       <main
         role="main"
         className="col-lg-12 mx-auto bg-dark bg-size-cover bg-repeat-0 bg-position-center position-relative overflow-hidden py-5 mb-4"
-        style={{ maxWidth: "2000px" }}
+        style={{ maxWidth: "1500px" }}
       >
         <div className="row">
           <div className="content mx-auto" style={{ marginTop: "250px" }}>
@@ -220,6 +219,167 @@ const Create = (artick) => {
           </div>
         </div>
       </main>
+      <footer className="footer pt-4 pb-4 pb-lg-5">
+        <div className="container pt-lg-4">
+          <div className="row pb-5">
+            <div className="col-lg-4 col-md-6">
+              <div className="navbar-brand text-dark p-0 me-0 mb-3 mb-lg-4">
+                <img
+                  src="assets/img/Transparent Logo.png"
+                  width="125"
+                  alt="Silicon"
+                />
+              </div>
+              <form className="needs-validation" noValidate>
+                <label htmlFor="subscr-email" className="form-label">
+                  Subscribe to our newsletter
+                </label>
+                <div className="input-group">
+                  <input
+                    type="email"
+                    id="subscr-email"
+                    className="form-control rounded-start ps-5"
+                    placeholder="Your email"
+                    required
+                  />
+                  <i className="bx bx-envelope fs-lg text-muted position-absolute top-50 start-0 translate-middle-y ms-3 zindex-5"></i>
+                  <div className="invalid-tooltip position-absolute top-100 start-0">
+                    Please provide a valid email address.
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="col-xl-6 col-lg-7 col-md-5 offset-xl-2 offset-md-1 pt-4 pt-md-1 pt-lg-0">
+              <div id="footer-links" className="row">
+                <div className="col-lg-4">
+                  <h6 className="mb-2">
+                    <a
+                      href="#useful-links"
+                      className="d-block text-dark dropdown-toggle d-lgclassName py-2"
+                      data-bs-toggle="collapse"
+                    >
+                      Useful Links
+                    </a>
+                  </h6>
+                  <div
+                    id="useful-links"
+                    className="collapse d-lg-block"
+                    data-bs-parent="#footer-links"
+                  >
+                    <ul className="nav flex-column pb-lg-1 mb-lg-3">
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          Home
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          Marketplace
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          About us
+                        </a>
+                      </li>
+                    </ul>
+                    <ul className="nav flex-column mb-2 mb-lg-0">
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          Terms &amp; Conditions
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          Privacy Policy
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-lg-3">
+                  <h6 className="mb-2">
+                    <a
+                      href="#social-links"
+                      className="d-block text-dark dropdown-toggle d-lgclassName py-2"
+                      data-bs-toggle="collapse"
+                    >
+                      Socials
+                    </a>
+                  </h6>
+                  <div
+                    id="social-links"
+                    className="collapse d-lg-block"
+                    data-bs-parent="#footer-links"
+                  >
+                    <ul className="nav flex-column mb-2 mb-lg-0">
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          LinkedIn
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          X
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#"
+                          className="nav-link d-inline-block px-0 pt-1 pb-2"
+                        >
+                          Instagram
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-lg-5 pt-2 pt-lg-0">
+                  <h6 className="mb-2">Contact Us</h6>
+                  <a href="mailto:email@example.com" className="fw-medium">
+                    Artick@gmail.com
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p className="nav d-block fs-xs text-center text-md-start pb-2 pb-lg-0 mb-0">
+            &copy; All rights reserved. Made by
+            <a
+              className="nav-link d-inline-block p-0"
+              href="https://createx.studio/"
+              target="_blank"
+              rel="noopener"
+            >
+              Createx Studio
+            </a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
